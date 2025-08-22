51c791a87b2ac977fee6aee2cb7755e84fb22ace
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-22 11:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
 
+// script.js
+
+const list = document.getElementById("infi-list");
+let itemCount = 1;
+
+// function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${itemCount++}`;
+    list.appendChild(li);
+  }
+}
+
+// add 10 items initially
+addItems(10);
+
+// add 2 more when scrolled to bottom
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
